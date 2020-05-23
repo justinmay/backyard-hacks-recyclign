e9f260480c04a1019004c1d2d84a6c79e28239db
Jahaan Turkhud <jturkhud2013@gmail.com>
2020-05-23 15:38:38 -0400

    Finished ashima scrapers

--- scrapers/ashima2_scraper.py
+++ scrapers/ashima2_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
-r = requests.get('https://www.theahimsacollective.com/collections/all?page=1')
+r = requests.get('https://www.theahimsacollective.com/collections/all?page=2')
 
 soup = BeautifulSoup(r.text, features="html.parser")
 
@@ -20,10 +20,10 @@ for product in product_link:
    links.append("https://www.theahimsacollective.com/"+product.find("a")['href'])
 
 ###  Finding Prices ### 
-product_price = soup.find_all("span", class_="money")
+product_price = soup.find_all("span", class_="price-item price-item--regular")
 prices = []
 for product in product_price:
-    prices.append(product.text)
+    prices.append(product.text.split(" ")[0][:-1])
 
 ###  Finding image links ### 
 product_image_link = soup.find_all("div", class_="grid-view-item product-card")
@@ -31,13 +31,13 @@ image_links = []
 for product in product_image_link:
    image_links.append("https://www.theahimsacollective.com/"+product.find("a")['href'])
 
-#print(len(names)) good
+# print(len(names)) 
 
-#print(len(prices)
+# print(len(prices))
 
-#print(len(links)) good
+# print(len(links))
 
-#print(len(image_links)) good
+# print(len(image_links)) 
 
 ### putting it all together ### 
 d = {'names': names, 'prices': prices, 'links': links, 'image_links': image_links}
